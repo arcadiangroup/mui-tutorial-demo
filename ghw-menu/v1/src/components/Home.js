011666708dc3ad6cbb7d63ebaec1dc730c6e6d3e
Michael Angerman <stormasm@gmail.com>
2018-08-08 16:48:20 -0700

    update markdownurl in Home

--- ghw-menu/v1/src/components/Home.js
+++ ghw-menu/v1/src/components/Home.js
@@ -1,9 +1,8 @@
 import React from "react";
 import MarkdownViewWrapper from "./../containers/MarkdownViewWrapper"
 
-
 const markdownurl =
-  "https://raw.githubusercontent.com/stormasm/mui-tutorial-demo/master/mui-menu/README.md";
+  "https://raw.githubusercontent.com/stormasm/mui-tutorial-demo/master/ghw-menu/README.md";
 
 const Home = () => (
   <div>
