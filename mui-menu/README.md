93ddf3d02716c260cc40624bf6c91216907dea2e
Michael Angerman <stormasm@gmail.com>
2018-08-08 11:56:49 -0700

    MarkdownViewWrapper up and running

--- mui-menu/README.md
+++ mui-menu/README.md
@@ -3,3 +3,5 @@
 
 Talk about
 [react-router-redux](https://github.com/ReactTraining/react-router/tree/master/packages/react-router-redux)
+
+Talk about the Admin functionality, adding keys, how it all works...
--- mui-menu/v1/src/components/Home.js
+++ mui-menu/v1/src/components/Home.js
@@ -1,8 +1,14 @@
 import React from "react";
+import MarkdownViewWrapper from "./../containers/MarkdownViewWrapper"
+
+
+const markdownurl =
+  "https://raw.githubusercontent.com/stormasm/mui-tutorial-demo/master/mui-menu/README.md";
 
 const Home = () => (
   <div>
     <h2>Home</h2>
+    <MarkdownViewWrapper view={markdownurl} />
   </div>
 );
 
